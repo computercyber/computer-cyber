09ede3f515d2ec495a68b89e1bcad2bec159e141
mdendi6@gmail.com <mdendi6@gmail.com>
2020-09-06 20:22:20 +0700

    v2.0

--- system/database/DB_driver.php
+++ system/database/DB_driver.php
@@ -690,7 +690,6 @@ abstract class CI_DB_driver {
 				// Display errors
 				return $this->display_error(array('Error Number: '.$error['code'], $error['message'], $sql));
 			}
-
 			return FALSE;
 		}
 
