4fb700d00be1516a060f920c1750ae7bdb783290
Dendi <mdendi@email.com>
2021-01-21 00:29:48 +0700

    navigasi desain

--- app/Controllers/Home.php
+++ app/Controllers/Home.php
@@ -1,12 +1,15 @@
-<?php namespace App\Controllers;
+<?php
+
+namespace App\Controllers;
 
 class Home extends BaseController
 {
 	public function index()
 	{
-		return view('welcome_message');
+		$data = [
+			'title' => 'Computer Cyber'
+		];
+
+		return view('layout/index', $data);
 	}
-
-	//--------------------------------------------------------------------
-
 }
---
+++ app/Views/layout/navbar.php
@@ -0,0 +1,100 @@
+<nav class="navbar navbar-expand-lg navbar-light bg-white">
+    <div class="container-fluid">
+
+        <a class="navbar-brand" href="<?= base_url(); ?>">Computer Cyber</a>
+
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+
+        <div class="collapse navbar-collapse" id="navbarNavDropdown">
+            <ul class="navbar-nav mx-auto">
+
+                <!-- mobile navbar -->
+                <div class="row mt-2 d-lg-none text-center">
+                    <div class="col-6">
+                        <li class="nav-item">
+                            <a class="nav-link active" aria-current="page" href="#">Home</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="#">Divisi</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="#">About Us</a>
+                        </li>
+                    </div>
+                    <div class="col-6">
+                        <li class="nav-item">
+                            <a class="nav-link" href="#">Karya</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                                Info
+                            </a>
+                            <ul class="dropdown-menu" style="border-left: 5px solid #5E31C2" aria-labelledby="navbarDropdownMenuLink">
+                                <li><a class="dropdown-item" href="#">Artikel</a></li>
+                                <li><a class="dropdown-item" href="#">Event dan Agenda</a></li>
+                                <li><a class="dropdown-item" href="#">News</a></li>
+                                <li><a class="dropdown-item" href="#">Kepengurusan</a></li>
+                            </ul>
+                        </li>
+                    </div>
+                </div>
+                <!-- end mobile navbar -->
+
+                <li class="nav-item d-none d-lg-block">
+                    <a class="nav-link active" aria-current="page" href="#">Home</a>
+                </li>
+                <li class="nav-item d-none d-lg-block">
+                    <a class="nav-link" href="#">Divisi</a>
+                </li>
+                <li class="nav-item d-none d-lg-block">
+                    <a class="nav-link" href="#">Karya</a>
+                </li>
+                <li class="nav-item dropdown d-none d-lg-block">
+                    <a class="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                        Info
+                    </a>
+                    <ul class="dropdown-menu" style="border-left: 5px solid #5E31C2" aria-labelledby="navbarDropdownMenuLink">
+                        <li><a class="dropdown-item" href="#">Artikel</a></li>
+                        <li><a class="dropdown-item" href="#">Event dan Agenda</a></li>
+                        <li><a class="dropdown-item" href="#">News</a></li>
+                        <li><a class="dropdown-item" href="#">Kepengurusan</a></li>
+                    </ul>
+                </li>
+                <li class="nav-item d-none d-lg-block">
+                    <a class="nav-link" href="#">About Us</a>
+                </li>
+
+            </ul>
+
+            <hr class="d-lg-none">
+
+            <ul class="navbar-nav mr-auto">
+                <!-- mobile -->
+                <div class="row d-lg-none text-center">
+                    <div class="col-6">
+                        <li class="nav-item">
+                            <a class="nav-link" href="#">sing up</a>
+                        </li>
+                    </div>
+                    <div class="col-6">
+                        <li class="nav-item">
+                            <a class="btn btn-primary" href="#">sing in</a>
+                        </li>
+                    </div>
+                </div>
+                <!-- end mobile  -->
+                <li class="nav-item d-none d-lg-block">
+                    <a class="nav-link" href="#">sing up</a>
+                </li>
+                <li class="nav-item d-none d-lg-block">
+                    <a class="btn btn-primary" href="#">sing in</a>
+                </li>
+            </ul>
+
+            <hr class="d-lg-none">
+
+        </div>
+    </div>
+</nav>
